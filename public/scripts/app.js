75cf50d181c664818497fcd1b29b817ba5463f02
Ryan White <ryanjwhite10@gmail.com>
2018-07-25 23:22:22 +0000

    Generate poll structure and drag and drop ordering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,33 @@
 //     }
 //   });;
 // });
+
+// Temp test data
+const data = {
+  title: 'My poll for stuff',
+  option1: 'option1',
+  option2: 'option2',
+  option3: 'option3',
+  option4: 'option4',
+  email: 'admin@example.com'
+};
+
+$(document).ready(function() {
+
+  function renderPoll(data) {
+    const $poll = $('.poll-container');
+    const $list = $('#sortable');
+    const $sortSpan = $('<span>').addClass('ui-icon ui-icon-arrowthick-2-n-s');
+
+    $('<li>').addClass('ui-state-default').text(data.option1).append($sortSpan).appendTo($list);
+    $('<li>').addClass('ui-state-default').text(data.option2).append($sortSpan).appendTo($list);
+    $('<li>').addClass('ui-state-default').text(data.option3).append($sortSpan).appendTo($list);
+    $('<li>').addClass('ui-state-default').text(data.option4).append($sortSpan).appendTo($list);
+  }
+
+  renderPoll(data);
+
+  $('#sortable').sortable();
+  $('#sortable').disableSelection();
+
+});

492d2edd7c3b31e60afff18a3bf77c72e08652cd
Ryan White <ryanjwhite10@gmail.com>
2018-07-26 02:44:53 +0000

    Implement ranking and scoring of sorted list on button click

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,13 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });;
+});
 
 // Temp test data
 const data = {
@@ -21,21 +21,43 @@ const data = {
 
 $(document).ready(function() {
 
+  // Sorts array of options and assigns points mased on position in array
+  function rankSortOptions(arr) {
+    const ranked = [];
+    arr = arr.reverse();
+
+    for (let i = 1; i <= arr.length; i++) {
+      let idx = arr.indexOf(i);
+      ranked.push(idx);
+    }
+
+    return ranked;
+  }
+
   function renderPoll(data) {
     const $poll = $('.poll-container');
     const $title = $('<h1>').text(data.title).appendTo($poll);
     const $list = $('<ul>').attr('id', 'sortable').appendTo($poll);
     const $sortSpan = $('<span>').addClass('ui-icon ui-icon-arrowthick-2-n-s');
 
-    $('<li>').addClass('ui-state-default').text(data.option1).append($sortSpan).appendTo($list);
-    $('<li>').addClass('ui-state-default').text(data.option2).append($sortSpan).appendTo($list);
-    $('<li>').addClass('ui-state-default').text(data.option3).append($sortSpan).appendTo($list);
-    $('<li>').addClass('ui-state-default').text(data.option4).append($sortSpan).appendTo($list);
+    $('<li>').addClass('ui-state-default').attr('id', '1').text(data.option1).append($sortSpan).appendTo($list);
+    $('<li>').addClass('ui-state-default').attr('id', '2').text(data.option2).append($sortSpan).appendTo($list);
+    $('<li>').addClass('ui-state-default').attr('id', '3').text(data.option3).append($sortSpan).appendTo($list);
+    $('<li>').addClass('ui-state-default').attr('id', '4').text(data.option4).append($sortSpan).appendTo($list);
+
+    $('<button>').addClass('rank-btn').text('Submit').appendTo($poll);
   }
 
   renderPoll(data);
 
-  $('#sortable').sortable({});
-  $('#sortable').disableSelection();
+  $('#sortable').sortable();
+
+  // Gets array of options in order that the user ranked them
+  $('.rank-btn').on('click' , function(event) {
+    let rankedOptions = $('#sortable').sortable('toArray');
+    rankedOptions = rankedOptions.map(function(option) { return Number(option); });
+    let rankedPoints = rankSortOptions(rankedOptions);
+    console.log(rankedPoints);
+  });
 
 });

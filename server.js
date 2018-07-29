c2a1b5a719d69c6b13241c175df81cc4685f0188
samueljohnwilson <samjwilson@gmail.com>
2018-07-29 19:40:07 +0000

    Modify server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 const PORT        = process.env.PORT || port;
-const ENV         = process.env.ENV || "development";
+const ENV         = process.env.ENV || "production";
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");

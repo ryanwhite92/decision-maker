a98637a287c5f13692b7747e0e84ef5d63f77f0e
samueljohnwilson <samjwilson@gmail.com>
2018-07-30 03:25:24 +0000

    Deploying to look at mailgun issue

--- routes/util/mailgun.js
+++ routes/util/mailgun.js
@@ -10,7 +10,7 @@ module.exports = {
     const ugly = '">Your Current Results</a></td></tr></tbody></table></td></tr></tbody></table><p align="center">dSide is a service that allows busy people to make group decisions with ease</p><p align="center">If you have a group decision to make, click <a href="https://boiling-meadow-35275.herokuapp.com/">here</a></p></td></tr></table></td></tr></table><div class="footer"><table border="0" cellpadding="0" cellspacing="0"><tr><td class="content-block"><span class="apple-link">The Basement<br>838 Fort St<br>Victoria<br>BC V8W 1H8</span><br> Is this an unwanted email? <a href="http://i.imgur.com/CScmqnj.gif">Unsubscribe</a></td></tr><tr></tr></table></div></div></td><td>&nbsp;</td></tr></table></body></html>'
     fs.appendFileSync(__dirname + "/email2.html", url);
     fs.appendFileSync(__dirname + "/email2.html", ugly)
-    const content = fs.readFileSync(__dirname + "/email2.html", "utf-8");
+    let content = fs.readFileSync(__dirname + "/email2.html", "utf-8");
     let mailgunSetup = {
       from: 'admin@dside.com',
       to: data.email,
@@ -23,12 +23,13 @@ module.exports = {
     });
   },
 
-    sendInvites: function(data) {
+  sendInvites: function(data) {
     const url = `https://boiling-meadow-35275.herokuapp.com/poll/${data.url}`
     let ugly = '">Cast your vote here</a></td></tr></tbody></table></td></tr></tbody></table><p align="center">dSide is a service that allows busy people to make group decisions with ease</p><p align="center">If you have a group decision to make, click <a href="https://boiling-meadow-35275.herokuapp.com/">here</a></p></td></tr></table></td></tr></table><div class="footer"><table border="0" cellpadding="0" cellspacing="0"><tr><td class="content-block"><span class="apple-link">The Basement<br>838 Fort St<br>Victoria<br>BC V8W 1H8</span><br> Is this an unwanted email? <a href="http://i.imgur.com/CScmqnj.gif">Unsubscribe</a></td></tr><tr></tr></table></div></div></td><td>&nbsp;</td></tr></table></body></html>'
     fs.appendFileSync(__dirname + "/email.html", url);
     fs.appendFileSync(__dirname + "/email.html", ugly)
-    const content = fs.readFileSync(__dirname + "/email.html", "utf-8");
+    let content = fs.readFileSync(__dirname + "/email.html", "utf-8");
+
     for (let i = 0; i < data.emails.length; i++) {
       let mailgunSetup = {
         from: 'admin@dside.com',

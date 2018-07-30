8cac585cb733da8a85429e88faa19bc2d2ba9647
samueljohnwilson <samjwilson@gmail.com>
2018-07-30 16:56:25 +0000

    Attempt to resolve display bug

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -188,4 +188,77 @@ $(document).ready(function() {
       }
     }
   });
+
+    function createPoll() {
+      $.ajax({
+        method: "GET",
+        url: "/api/users" + window.location.pathname
+      }).done((table) => {
+          let $results = $(".results");
+          let optionArr = [];
+          for (let i = 0; i < table.options.length; i++) {
+            optionArr.push(table.options[i])
+          }
+
+          if (table.ranks.length < 1) {
+            table.ranks = [1,1]
+          }
+
+          document.chart = new Highcharts.Chart({
+            chart: {
+              type: 'column',
+              renderTo: $results[0]
+            },
+
+            colors: ["#061539"],
+
+            title: {
+              text: "Here's the current results for:"
+            },
+            subtitle: {
+              text: table.question
+            },
+            xAxis: {
+              categories: optionArr
+            },
+            yAxis: {
+              title: {
+                text: "Votes"
+              },
+              labels: {
+                enabled: false
+              }
+            },
+            series: [{
+              showInLegend: false,
+              data: table.ranks
+            }]
+          });
+      });
+  }
+
+  function renderEmailList() {
+    let path = window.location.pathname;
+    path = path.replace("results", "response");
+
+    $.ajax({
+      method: "GET",
+      url: "/api/users" + path
+    }).done(function(responses) {
+      if (responses.length > 0) {
+        const total = responses.length;
+        const $results = $(".results");
+        const $list = $('<ul>').addClass("unstyled").text("Votes: " + total).appendTo($results);
+
+        for (let i = 0; i < responses.length; i++) {
+          let email = responses[i].email;
+          $('<li>').text(email).appendTo($list);
+        }
+      }
+    });
+  }
+
+  createPoll();
+  renderEmailList();
+  
 });
